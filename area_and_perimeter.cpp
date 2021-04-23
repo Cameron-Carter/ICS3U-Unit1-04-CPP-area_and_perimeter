bac186b9dbfd0be24873e2621acf5f89cc730e4f
Cameron Carter <cameron.carter@mths.ca>
2021-04-23 22:53:37 +0000

    minor adjustments

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Cameron Carter All rights reserved
+//
+// Created by Cameron Carter
+// Created on April 2021
+// This is a simple "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the following dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "The area is " << (5 * 3) << "cm^2." << std::endl;
+    std::cout << "The perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+}
